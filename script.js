2809efb19f4badc232903c4bfb2d161e65aba2ad
Colin Merkert <c.merkert@outlook.de>
2020-05-26 17:27:38 +0200

    added comments

--- script.js
+++ script.js
@@ -3,25 +3,25 @@
 */
 
 //Global Variables
-let playerPoints = 4;
-let computerPoints = 4;
+let playerPoints = 0;
+let computerPoints = 0;
 let score = document.getElementById("score");
 
 //Initialise Score on Page
 score.innerText = `Player ${playerPoints} : ${computerPoints} Computer`;
 
-// The Game
+// The Game put together 
 
 function game() {
     
 
-    while (playerPoints < 5 && computerPoints < 5 ) {
+    while (playerPoints < 5 && computerPoints < 5 ) {                   //plays until 5 points are reached
         
-        let playerSelection = convertPlayerChoice(getPlayerChoice());
-        let computerSelection = computerPlay();
+        let playerSelection = convertPlayerChoice(getPlayerChoice());   //gets input from the player and converts it to uppercase
+        let computerSelection = computerPlay();                         //randomly asigns a choice of rock, paper or scissors
 
-        playRound(playerSelection, computerSelection);
-        updateScore(playerPoints, computerPoints);
+        playRound(playerSelection, computerSelection);                  //evaluates who wins
+        updateScore(playerPoints, computerPoints);                      //updates the p tag on the page
     }
         
 }
@@ -29,12 +29,6 @@ function game() {
 function updateScore(pPoints, cPoints) {
     score.innerText = `Player ${pPoints} : ${cPoints} Computer`;
 }
-
-
-
-
-
-
 
 //Convert the random Number to a usable String
 function computerPlay() {
@@ -80,7 +74,7 @@ function random(value) {
     return Math.floor(Math.random() * Math.floor(value));
 }
 
-//Evaluates if the Player WINS, LOSES or if it's a DRAW
+//Evaluates if the Player WINS, LOSES or if it's a DRAW and increments the points
 function playRound(playerSelection, computerSelection) {
 
     if (checkPlayerChoice(playerSelection)){
